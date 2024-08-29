79e82fcee747a1e50ca0f9b95d6a8377eefebac3
jjmccarty <jessica@mccarty-online.com>
2024-08-29 18:47:53 -0500

    Initial data load and files converted to dataframes

---
+++ data_clean.py
@@ -0,0 +1,65 @@
+# Dependencies
+import requests
+import time
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import json
+import sys
+import pathlib
+import csv
+import datetime
+
+
+def getDiabetesDF_OverallCrude():
+    df = getDataFrameForFile('overall_crude')
+    #do cleaning work here
+    print(df.columns)
+    return df 
+        
+def getDiabetesDF_OverallAgeAdjusted():
+    df = getDataFrameForFile('overall_ageadjusted')
+    #do cleaning work here
+    return df 
+
+def getDiabetesDF_BySex_Crude():
+    df = getDataFrameForFile('female_crude')
+    #do cleaning work here
+    return df 
+
+def getDiabetesDF_BySex_AgeAdjusted():
+    df = getDataFrameForFile('female_ageadjusted')
+    #do cleaning work here
+    return df 
+
+def getDiabetesDF_ByAge():
+    df = getDataFrameForFile('age_18to44')
+    #do cleaning work here
+    return df 
+
+def getDiabetesDF_ByEthnicity():
+    df = getDataFrameForFile('ethno_asian_crude')
+    #do cleaning work here
+    return df 
+
+def getDataFrameForFile(filename):
+    print(f'----> Retrieving information for {filename}')
+    df_list = []
+    for year in range(2019, 2023):
+        filepath = f'../Resources/diabetes_{str(year)}_{filename}.csv'
+        print(filepath)
+        df_list.append(pd.read_csv(filepath))
+
+    df = pd.concat(df_list, join='outer')
+    return df
+
+
+if __name__ == '__main__':
+    print('Run data_clean.py')
+
+    #print(getDiabetesDF_OverallCrude().info())
+    #print(getDiabetesDF_OverallAgeAdjusted().info())
+    #print(getDiabetesDF_ByEthnicity().info())
+    #print(getDiabetesDF_BySex_Crude().info())
+    #print(getDiabetesDF_BySex_AgeAdjusted().info())
+    #print(getDiabetesDF_ByAge().info())
