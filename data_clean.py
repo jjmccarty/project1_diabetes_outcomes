341ccda93eba4b62e33da738c2a382c6dfe8667d
jjmccarty <jessica@mccarty-online.com>
2024-08-29 19:38:59 -0500

    cleaned out unnecessary columns

--- data_clean.py
+++ data_clean.py
@@ -14,33 +14,55 @@ import datetime
 def getDiabetesDF_OverallCrude():
     df = getDataFrameForFile('overall_crude')
     #do cleaning work here
-    print(df.columns)
+    cleanBasicData(df)
     return df 
         
 def getDiabetesDF_OverallAgeAdjusted():
     df = getDataFrameForFile('overall_ageadjusted')
     #do cleaning work here
+    cleanBasicData(df)
     return df 
 
 def getDiabetesDF_BySex_Crude():
     df = getDataFrameForFile('female_crude')
     #do cleaning work here
+    cleanBasicData(df)
     return df 
 
 def getDiabetesDF_BySex_AgeAdjusted():
     df = getDataFrameForFile('female_ageadjusted')
     #do cleaning work here
+    cleanBasicData(df)
     return df 
 
 def getDiabetesDF_ByAge():
     df = getDataFrameForFile('age_18to44')
     #do cleaning work here
+    cleanBasicData(df)
     return df 
 
 def getDiabetesDF_ByEthnicity():
     df = getDataFrameForFile('ethno_asian_crude')
     #do cleaning work here
+    cleanBasicData(df)
     return df 
+
+def cleanBasicData(df):
+    print(df.columns)
+    df.drop(columns=['LocationDesc', 
+                     'TopicID', 
+                     'DataSource', 
+                     'DataSourceUrl', 
+                     'Question', 
+                     'QuestionID', 
+                     'StratificationCategory', 
+                     'DataValueUnit', 
+                     'DisplayOrder', 
+                     'DataValueFootnoteSymbol'], inplace=True)
+    
+    print(df.columns)
+
+
 
 def getDataFrameForFile(filename):
     print(f'----> Retrieving information for {filename}')
@@ -58,8 +80,8 @@ if __name__ == '__main__':
     print('Run data_clean.py')
 
     print(getDiabetesDF_OverallCrude().info())
-    print(getDiabetesDF_OverallAgeAdjusted().info())
-    print(getDiabetesDF_ByEthnicity().info())
-    print(getDiabetesDF_BySex_Crude().info())
-    print(getDiabetesDF_BySex_AgeAdjusted().info())
-    print(getDiabetesDF_ByAge().info())
+    #print(getDiabetesDF_OverallAgeAdjusted().info())
+    #print(getDiabetesDF_ByEthnicity().info())
+    #print(getDiabetesDF_BySex_Crude().info())
+    #print(getDiabetesDF_BySex_AgeAdjusted().info())
+    #print(getDiabetesDF_ByAge().info())
